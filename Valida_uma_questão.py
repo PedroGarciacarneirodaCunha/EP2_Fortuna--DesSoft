3233c2f170031d0b8a928a42a0d136b78d2c573d
PedroGarciacarneirodaCunha <pedrogcc@al.insper.edu.br>
2022-07-29 23:53:41 -0300

    func

--- Valida_uma_questão.py
+++ Valida_uma_questão.py
@@ -6,43 +6,51 @@ def valida_questao(dict_quest):
 
     ABCD = ['A', 'B', 'C', 'D']
 
+    l_nivel = ['facil', 'medio', 'dificil']
+
     for nome in lista_nomes:
         
         if nome in dict_quest.keys():
             pass
         else:
-            dict_saida['titulo'] = 'nao_encontrado'
+            dict_saida[nome] = 'nao_encontrado'
         
     if len(dict_quest.keys()) == 4:
         pass
     else:
         dict_saida['outro'] = 'numero_chaves_invalido'
         
-    if dict_quest.keys() == 'titulo' and dict_quest['titulo'] != ' ' or dict_quest['titulo'] != '':
+    if 'titulo' in dict_quest.keys() and len(str(dict_quest['titulo']).strip()) != 0:
         pass
     else:
         dict_saida['titulo'] = 'vazio'
         
-    if 'nivel' in dict_quest.keys() and dict_quest['nivel'] == ('facil' or 'medio' or 'dificil'):
+    if 'nivel' in dict_quest.keys() and dict_quest['nivel'] in l_nivel:
         pass
     else:
         dict_saida['nivel'] = 'valor_errado'
         
-    if dict_quest.keys() == 'opcoes' and len(dict_quest['opcoes']) != 4:
+    if 'opcoes' in dict_quest.keys() and len(dict_quest['opcoes']) != 4:
 
         dict_saida['opcoes'] = 'tamanho_invalido'
     
     else:
 
+        dict_vaz = {}
+
         for abcd in ABCD:
 
-            if abcd not in dict_quest['opcoes'].keys():
+            if abcd in dict_quest['opcoes'].keys():
                 pass
             else:
                 dict_saida['opcoes'] = 'chave_invalida_ou_nao_encontrada'
 ## parte da resposta vazia
-            ##if dict_quest['opcoes'[abcd]] == '' or dict_quest['opcoes'[abcd]] == ' ':
-                ##dict_saida['opcoes'] = {dict_quest['opcoes'[abcd]]: 'vazia'}
+
+            if len(str(dict_quest['opcoes'][abcd]).strip()) == 0:
+                dict_vaz[abcd] = 'vazia'
+            
+            if len(dict_vaz) > 0:
+                dict_saida['opcoes'] = dict_vaz
         
     if 'correta' in dict_quest.keys() and dict_quest['correta'] in ABCD:
         pass
